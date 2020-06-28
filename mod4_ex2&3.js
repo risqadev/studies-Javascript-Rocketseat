bd76675495254e70aea9136783fe08ea2a535e40
Ricardo Camacho <camacho.rc@gmail.com>
2020-06-28 19:54:00 -0300

    Module 4 exercise 3 completed

--- mod4_ex2&3.js
+++ mod4_ex2&3.js
@@ -8,10 +8,14 @@ console.log(buttonElement);*/
 
 let renderListRepos = () => {
   let username = inputElement.value;
-  listElement.innerHTML = '';
+  // let username = 'rscamacho';
+
+  listElement.innerHTML = '<li>Carregando...</li>';
 
   axios.get(`https://api.github.com/users/${username}/repos`)
     .then( (response) => {
+      listElement.innerHTML = '';
+
       for( repo of response.data) {
         let pos = response.data.indexOf(repo);
 
@@ -22,6 +26,12 @@ let renderListRepos = () => {
 
         itemElement.appendChild(repoName);
       }
-    } )
-    .catch( (error) => console.log(error) );
+    })
+    .catch( (error) => {
+      if (error.response.status === 404) {
+        listElement.innerHTML = '<li>Usuário não encontrado (404)</li>';
+      } else {
+        listElement.innerHTML = '<li>Ocorreu um erro.</li>';
+      }
+    });
 }
