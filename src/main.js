f4f27f99c0e0b6136257b0ead86b03e5ef70cfb1
Ricardo Camacho <camacho.rc@gmail.com>
2020-07-04 22:15:50 -0300

    Mod. 3, lesson 1 - async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
-alert('Teste');
+const myPromise1 = () => new Promise( (resolve, reject) => {
+  setTimeout( () => { resolve('OK') } , 1000);
+});
+
+/*myPromise1()
+  .then( response => {
+    console.log('myPromise1 resolved: ' + response);
+  })
+  .catch( err => {
+    console.log('myPromise1 error: ' + err);
+  }); */
+
+async function execPromise1() {
+  console.log('execPromise1() started');
+
+  const response = await myPromise1();
+    console.log(response);
+  
+  console.log(await myPromise1());
+  console.log(await myPromise1());
+  console.log('Go ahead? No.');
+  console.log(await myPromise1());
+
+  console.log('execPromise1() completed');
+}
+execPromise1();
+
+// sem async/await ela não espera a conclusão da promise e irá retornar antes de execPromise1, mesmo tendo sido chamada depois
+function execPromise2() {
+  console.log('execPromise2() started');
+
+  const response = myPromise1();
+  console.log(response);
+
+  console.log('execPromise2() completed');
+}
+execPromise2();
+
+// essa função também retornará antes
+const anotherFunction = () => console.log('Another function.');
+anotherFunction();
