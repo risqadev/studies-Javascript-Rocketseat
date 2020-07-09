460be207fa8d3cecb8559240ca22b1571392d56e
Ricardo Camacho <camacho.rc@gmail.com>
2020-07-08 23:00:11 -0300

    Mod. 1, exercise 5 completed.

--- mod_1/mod1_ex4.js
+++ mod_1/mod1_ex4.js
@@ -21,3 +21,37 @@ function mostraInfo( { nome, idade } ) {
 console.log(
   mostraInfo( { nome: 'Diego', idade: 23 } )
 );
+
+// 5.1
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [ x, ...y ] = arr;
+
+console.log(x);
+console.log(y);
+
+function soma(a, ...arr) {
+  if (arr.length > 0) {
+    a += soma(...arr);
+  }
+  return a;
+}
+console.log( soma(1, 2, 3, 4, 5, 6) );
+console.log( soma(1, 2) );
+
+// 5.2
+const usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+  }
+};
+
+let usuario2 = {...usuario, nome: 'Gabriel' };
+console.log(usuario2);
+
+let usuario3 = {...usuario, endereco: {...usuario.endereco, cidade: 'Lontras'} };
+console.log(usuario3);
